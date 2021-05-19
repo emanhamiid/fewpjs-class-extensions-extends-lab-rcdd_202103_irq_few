607b8bba58f82d63c8c2e91bbc89ee597c7d20e1
eman hamid <emanhharki@gmail.com>
2021-05-19 21:04:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+class Polygon{
+  construct(){
+new polygon = beforeEach( () => {
+    polygon = new Polygon( [ 5, 5, 5 ] )
+  } )
+  }
+}
+class Triangle extends Polygon{
+  construct(){
+
+  }
+}
